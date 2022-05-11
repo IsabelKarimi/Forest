25a0dae1cbf5c5e06265a98d2050da373fd4f35f
Isabel Karimi <100405131+IsabelKarimi@users.noreply.github.com>
2022-05-11 18:24:12 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,50 +1,83 @@
+import modules.EndagereredSpecies;
+import modules.Sighten;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.security.Timestamp;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.*;
 
 import static spark.Spark.*;
-
 public class App {
-    public static void main(String[] args) {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args){
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
-
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "index.hbs");
+        get("/", (request, response) ->{
+            Map<String, Object > model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.html");
         }, new HandlebarsTemplateEngine());
-
-
-        get("/animal", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            ArrayList<Animal> animals=Animal.getmInstance();
-            model.put("animals",animals);
-            return new ModelAndView(model, "sightingform.hbs");
+        get("/sighting",(request, response) -> {
+            Map<String, Object > model = new HashMap<String, Object>();
+            return new ModelAndView(model, "preSighting.html");
         }, new HandlebarsTemplateEngine());
-
-        post("/animal/new", (request, response) -> {
+        post("/sighten/frm",(request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             String name = request.queryParams("name");
-            String age = request.queryParams("age");
             String health = request.queryParams("health");
+            System.out.println(health);
+            String location = request.queryParams("locationEndager");
+            String ranger = request.queryParams("ranger");
+            String age = request.queryParams("age");
+            Endagerered newAnimal = new Endagerered(name, health, age);
+            newAnimal.save();
+            Sighten sighten= new Sighten(ranger,location,newAnimal.getId());
+            sighten.save();
+            model.put("newAnimal", newAnimal);
+            return new ModelAndView(model,"success.hbs");
+//
+        },new HandlebarsTemplateEngine());
+      get("/sighten/main",(request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        List<Endagerered>all= Endagerered.getAll();
+        List<Sighten>getInfo= Sighten.getAll();
+        ArrayList<String>name=new ArrayList<>();
+        ArrayList<String> health= new ArrayList<>();
+        ArrayList<String>age = new ArrayList<>();
+        ArrayList<String>ranger = new ArrayList<>();
+        ArrayList<String>location = new ArrayList<>();
+        ArrayList<Timestamp> time = new ArrayList<>();
+        for(Endagerered: all){
+            name.add(animal.getName());
+            health.add(animal.getHEALTH());
+            age.add(animal.getAGE());
+        }
+        for(Sighten sight: getInfo){
+            ranger.add(sight.getRanger());
+            location.add(sight.getLOCATION());
+            time.add(sight.getLastSeen());
+        }
+      model.put("all",all);
+        model.put("name",name);
 
-            Animal animals = new Animal(name,age,health);
-            model.put("animals" ,animals);
-            System.out.println(request.queryParams("name"));
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
+        model.put("health", health);
+        model.put("age", age);
+        model.put("ranger", ranger);
+        model.put("location", location);
+        model.put("time", time);
 
-        get("/animals/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            ArrayList<Animal> animals=Animal.getmInstance();
-            model.put("animals",animals);
-            return new ModelAndView(model, "animals.hbs");
-        }, new HandlebarsTemplateEngine());
+        
 
-
+//
+        return (new ModelAndView(model,"sighting.hbs"));
+      }, new HandlebarsTemplateEngine());
 
     }
-
 }
