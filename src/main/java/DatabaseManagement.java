7bbee182bf04f5958db622af17193d258e3d6e83
Isabel Karimi <100405131+IsabelKarimi@users.noreply.github.com>
2022-05-11 18:22:16 +0300

    Update DatabaseManagement.java

--- src/main/java/DatabaseManagement.java
+++ src/main/java/DatabaseManagement.java
@@ -1,9 +1,7 @@
-
+package interfaces;
 
 public interface DatabaseManagement {
+    public  void save();
+    public  void delete(int id);
 
-    boolean equals(Object otherObject);
-
-    void save();
 }
-
